1bfb8a6387b38a6aa947da8b377f52930f9fa640
GRuslan <garagonuch@mail.ru>
2015-04-28 18:46:34 +0300

    Add recognition face method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,17 @@ class StaticPagesController < ApplicationController
   end
 
   def help
+  flash[:success] = face_recognition("/home/ruslan/me.jpg", "/home/ruslan/you.jpg");
   end
 
   def about
   end
 
   def contact
+  end
+
+  def face_recognition(savedFoto, newFoto)
+    i = `br -algorithm FaceRecognition -compare #{savedFoto} #{newFoto}`
+  return i
   end
 end
